04d563f55b5a269941f79fb790bb4408f8a8788c
ArturoOliGal <arturoolivaresgalllegos@gmail.com>
2025-02-06 20:34:38 -0600

    Portfolio fase 1

--- portfolio/index.py
+++ portfolio/index.py
@@ -6,12 +6,23 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('aboutme.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
+
+@app.route('/dataAnalyst')
+def dataAnalyst():
+    return render_template('dataAnalyst.html')
+
+@app.route('/projectManager')
+def projectManager():
+    return render_template('projectManager.html')
 
 if __name__=='__main__':
     app.run(debug=True)
